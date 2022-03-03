28ce9b7c8b92a2dde73b548173fd93999d1eb197
Bhupesh <72209863+bhupesshhh@users.noreply.github.com>
2022-03-03 21:44:45 +0530

    Add files via upload

---
+++ Sec-1-1-Mathematics-Expressions.R
@@ -0,0 +1,23 @@
+### Mathematics Expressions
+
+# Addition
+4+5
+
+#Subtraction
+7-2
+5-9
+
+#Division
+3/4
+
+#Multiplication
+4*5
+
+#Power/Exponent
+4^4
+
+#Modulus/Remainder
+8%%3
+
+#Order of Operations
+(100 * 2) + (50 / 2)
---
+++ Sec-1-3-Data-Types.R
@@ -0,0 +1,23 @@
+### Data Types
+
+#Numeric : Can be double, integer, imaginary
+value.first <- 2.2222
+value.first
+value.second <- 123L
+value.second
+value.third <- 3+4i
+value.third
+
+#Logical
+var.true <- TRUE
+var.false <- FALSE
+
+#Characters : 
+# Double Quotes is preferred convention
+# Connecting variable with dost is preferred convention
+var.name <- "bhupesh"
+
+#Check Data Types
+class(value.first)
+class(value.second)
+class(value.third)
